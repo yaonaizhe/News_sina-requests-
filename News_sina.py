66510013d7ea1276b3e00ffc93e6e1d35701aeff
张大仙 <zt775327332@gmail.com>
2019-02-24 20:01:54 +0800

    Update News_sina.py

--- News_sina.py
+++ News_sina.py
@@ -11,17 +11,20 @@ from config import *
 class News_sina():
 
     def __init__(self):
-        self.start_url = 'http://news.sina.com.cn/guide/'
-        self.now = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+        self.start_url = 'http://news.sina.com.cn/guide/'   # 起始URL，新浪新闻导航
+        self.now = time.strftime('%Y-%m-%d', time.localtime(time.time()))   # 获取当前时间
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
         }
-        self.table = 'news'
-        self.dct1 = {}
-        self.dct2 = {}
-        self.dct3 = {}
+        self.dct1 = {}  # 存储类名及对应的URL
+        self.dct3 = {}  # 存储最终结果：新闻标题，新闻内容，新闻类型，发布时间，来源
 
     def download(self, url):
+        '''
+        发送请求，获取响应
+        :param url:
+        :return:
+        '''
         try:
             response = requests.get(url, headers=self.headers)
             if response.status_code == 200:
@@ -31,6 +34,11 @@ class News_sina():
             print('%s + download + %s' % (e, url))
 
     def parse1(self, response):
+        '''
+        解析首页，获取新闻类别及URL
+        :param response:
+        :return:
+        '''
         try:
             html = etree.HTML(response.text)
             links = html.xpath('//div[@id="tab01"]//ul/li/a/@href')
@@ -42,6 +50,11 @@ class News_sina():
             print('%s + parse1 + %s' % (e, response.url))
 
     def parse2(self, response):
+        '''
+        解析每个新闻类别网页，获取当天发布的新闻链接
+        :param response:
+        :return:
+        '''
         try:
             html = etree.HTML(response.text)
             links = html.xpath('//a/@href')
@@ -54,6 +67,12 @@ class News_sina():
             print('%s + parse2 + %s' % (e, response.url))
 
     def parse3(self, response, type):
+        '''
+        解析新闻详情页面，获取新闻标题，新闻内容
+        :param response:
+        :param type:
+        :return:
+        '''
         try:
             html = etree.HTML(response.text)
             title = html.xpath('//h1/text() | //h2/text()')[0]
@@ -70,9 +89,14 @@ class News_sina():
             print('%s + parse3 + %s' % (e, response.url))
 
     def save(self, item):
+        '''
+        将结果保存到Mysql数据库中
+        :param item:
+        :return:
+        '''
         keys = ','.join(item.keys())
         values = ','.join(['%s']*len(item))
-        sql = 'insert into {table}({keys}) values({values})'.format(table=self.table, keys=keys, values=values)
+        sql = 'insert into {table}({keys}) values({values})'.format(table=table, keys=keys, values=values)
         try:
             if cursor.execute(sql, tuple(item.values())):
                 print('Successful')
@@ -82,8 +106,12 @@ class News_sina():
             print(e)
             db.rollback()
 
-
     def del_with_content(self, content):
+        '''
+        对新闻内容稍作处理
+        :param content:
+        :return:
+        '''
         new_content = []
         for i in content:
             i = i.strip()
